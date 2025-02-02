e6faa89389ceaea7723f421354d45af42814ed05
achung123 <ajoshpc@gmail.com>
2025-02-02 02:30:36 -0500

    Add more interesting endpoints...

--- src/app/database/database.py
+++ src/app/database/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./test.db"
-
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+# Database setup
+SQLALCHEMY_DATABASE_URL = "sqlite:///./poker.db"
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- src/app/routes/game.py
+++ src/app/routes/game.py
@@ -1,18 +1,22 @@
 from fastapi import APIRouter
 
+from pydantic_models.app_models import Hand
+
 router = APIRouter()
 
 
-@router.get("/")
-def get_users():
-    return {"message": "List of users"}
-
-
-@router.get("/{user_id}")
-def get_user(user_id: int):
-    return {"message": f"User {user_id}"}
-
-
 @router.post("/")
-def create_user():
-    return {"message": "User created"}
+def create_game():
+    return {"message": "New Game Created"}
+
+
+@router.get("/game/{game_id}")
+def get_game(game_id: int):
+    # TODO: Implement this endpoint
+    return {"message": f"Game {game_id}"}
+
+
+@router.post("/hand/{user_id}")
+def push_hand(user_id: int, hand: Hand):
+    print(hand)  # noqa: T201
+    return {"message": f"Hand {hand} for user {user_id} pushed"}
