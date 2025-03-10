d00851912843eee6edf019f53a71fb859dd57f2a
Gil Felix <gfelix316@gmail.com>
2025-03-09 23:56:25 -0400

    updated game post

--- src/app/routes/game.py
+++ src/app/routes/game.py
@@ -19,9 +19,53 @@ SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
 
 
 @router.post("/")
-def create_game():
-    return {"message": "New Game Created"}
+def create_game(db: Annotated[Session, Depends(get_db)]):
+    """
+    Create a new game table
+    """
+    today = datetime.datetime.now(datetime.timedelta("America/New_York")).date()
+    formatted_date = today.strftime("%m-%d-%Y")
+    games = (
+        db.query(database.database_models.Game)
+        .filter(database.database_models.Game.game_date == formatted_date)
+        .all()
+    )
+    if games:
+        raise HTTPException(status_code=404, detail="Game already exists...")
 
+    game_entry = database.database_models.Game(
+        game_date=formatted_date, winner="Gil", losers="Adam,Matt,Zain"
+    )
+    db.add(game_entry)
+    db.commit()
+
+    queried_game = (
+        db.query(database.database_models.Game)
+        .filter(database.database_models.Game.game_date == formatted_date)
+        .first()
+    )
+
+
+# Endpoint to get a game by id
+@router.get("/")
+def get_game_by_date(db: Session = Depends(get_db), request: str = Depends(GameRequest)):
+    """
+    Get a game by date
+    """
+    game = (
+        db.query(database.database_models.Game)
+        .filter(database.database_models.Game.game_date == request.game_date)
+        .first()
+    )
+    if game is None:
+        raise HTTPException(status_code=404, detail="Game not found")
+    
+    return GameResponse(
+        game_id=game.game_id,
+        game_date=game.game_date,
+        winner=game.winner,
+        losers=game.losers,
+    )
 
 @router.get("/{game_id}")
 def get_game(game_id: int):
@@ -176,29 +220,3 @@ def get_community(game_date: str, hand_number: int):
         )
         return response.model_dump()
 
-# Endpoint to Update winners and losers
-# @router.put("/{game_id}")
-# def update_game(
-#     db: Annotated[Session, Depends(get_db)], game_id: int, game: pydantic_models.Game
-# ):
-#     """
-#     Update a game by ID
-#     """
-#     db_game = (
-#         db.query(database.database_models.Game)
-#         .filter(database.database_models.Game.game_id == game_id)
-#         .first()
-#     )
-#     if db_game is None:
-#         raise HTTPException(status_code=404, detail="Game not found")
-
-#     db_game.winner = game.winner
-#     db_game.losers = game.losers
-#     db.commit()
-
-#     return pydantic_models.GameResponse(
-#         game_id=db_game.game_id,
-#         game_date=db_game.game_date,
-#         winner=db_game.winner,`z`
-#         losers=db_game.losers,
-#     )
